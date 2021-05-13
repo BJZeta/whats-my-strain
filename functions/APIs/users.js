773ba47d74ffba1f0ee097385f2997b4066610b7
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-05-12 17:06:08 -0700

    adding api route to allow users to upload profile pic

--- functions/APIs/users.js
+++ functions/APIs/users.js
@@ -6,7 +6,8 @@ firebase.initializeApp(config);
 
 const { validateLoginData, validateSignUpData } = require("../util/validators");
 
-////// LOGIN
+///////////////////////////// LOGIN
+////////////////////// POST Request
 exports.loginUser = (req, res) => {
   const user = {
     email: req.body.email,
@@ -31,7 +32,8 @@ exports.loginUser = (req, res) => {
     });
 };
 
-////SIGNUP
+///////////////////////////// SIGNUP
+/////////////////////// POST Request
 exports.signUpUser = (req, res) => {
   const newUser = {
     username: req.body.username,
@@ -85,4 +87,72 @@ exports.signUpUser = (req, res) => {
           .json({ general: "Something wen wrong, please try again..." });
       }
     });
+};
+
+///////////////// UPLOAD PROFILE PIC
+/////////////////////// POST Request
+
+///deleteImage function
+deleteImage = (imageName) => {
+  const bucket = admin.storage().bucket();
+  const path = `${imageName}`;
+  return bucket
+    .file(path)
+    .delete()
+    .then(() => {
+      return;
+    })
+    .catch((err) => {
+      return;
+    });
+};
+
+exports.uploadProfilePic = (req, res) => {
+  const BusBoy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+
+  const busboy = new BusBoy({ headers: req.headers });
+  let imageFileName;
+  let imageToBeUpload = {};
+
+  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    if (mimetype !== "image/png" && mimetype !== "image/jpeg" && mimetype !== "image/jpg") {
+      return res.status(400).json({ error: "Wrong file submitted" });
+    }
+    const imageExtension = filename.split(".")[filename.split(".").length - 1];
+    imageFileName = `${req.user.username}.${imageExtension}`;
+    const filePath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUpload = { filePath, mimetype };
+    file.pipe(fs.createWriteStream(filePath));
+  });
+  deleteImage(imageFileName);
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUpload.filePath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUpload.mimetype,
+          },
+        },
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+        return db.doc(`/users/${req.user.username}`).update({
+          imageUrl,
+        });
+      })
+      .then(() => {
+        return res.json({ message: "Image Uploaded!" });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  });
+  busboy.end(req.rawBody);
 };
--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,14 @@
 const functions = require("firebase-functions");
 const app = require("express")();
+const auth = require("./util/auth");
 
-const { loginUser, signUpUser } = require("./APIs/users");
+const { loginUser, signUpUser, uploadProfilePic } = require("./APIs/users");
 
 const { getAllStrains } = require("./APIs/strains");
 
 app.post("/signup", signUpUser);
 app.post("/login", loginUser);
+app.post("/user/image", auth, uploadProfilePic);
 app.get("/strains", getAllStrains);
 
 exports.api = functions.https.onRequest(app);
