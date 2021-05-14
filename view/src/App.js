23ac6cabba7614be5308321ca229f96e8ca85dce
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-05-13 20:10:47 -0700

    adding landing page

--- view/src/App.js
+++ view/src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import LandingScreen from "./pages/LandingScreen";
 import "./bootstrap.min.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello Weed</h1>
-    </div>
+    <Router>
+      <Container className="pt-5">
+        <Route path="/" component={LandingScreen} exact />
+      </Container>
+    </Router>
   );
 }
 
---
+++ view/src/pages/LandingScreen.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Button, Row, Image } from "react-bootstrap";
+// import styled from "styled-components";
+import potlogo from "../assets/potlogo.gif";
+
+const LandingScreen = () => {
+  return (
+    <>
+      <Row className="text-center">
+        <h1>What's My Strain???&trade;</h1>
+        <Image src={potlogo} height="360" width="150" />
+        <h3 className="mt-2 mb-5">
+          Find out what cannabis strain fits right for you through simple
+          questions
+        </h3>
+        <div className="mt-5">
+          <LinkContainer to="/">
+            <Button size="lg" as="button" className="btn-success">
+              Start Fresh
+            </Button>
+          </LinkContainer>
+          <LinkContainer to="/">
+            <Button size="lg" as="button" className="mx-auto">
+              Sign In
+            </Button>
+          </LinkContainer>
+        </div>
+      </Row>
+    </>
+  );
+};
+
+export default LandingScreen;
